de85c55fa2f343acecdfb1a1bfe13c8af8d7af1a
sally <sallyakrum83@gmail.com>
2019-05-08 13:54:28 -0500

    added new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from "react";
-import Search from "./components/Search";
-import "./App.css";
-
-
-      
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
-  return <Search />;
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
